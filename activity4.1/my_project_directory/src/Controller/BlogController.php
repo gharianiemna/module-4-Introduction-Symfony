262bfb94b85377ebadc75cff7dd1bbf6aa81d077
Emna Ghariani <eghariani@dom.tti>
2022-06-29 16:15:59 +0100

    act 4.1 part 3 updates

--- activity4.1/my_project_directory/src/Controller/BlogController.php
+++ activity4.1/my_project_directory/src/Controller/BlogController.php
@@ -8,6 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+
+        /**
+     * @Route("/", name="home")
+     */
+    public Function home() {
+        return $this->render('blog/home.html.twig', [
+          'title' => "Bienvenue",
+          'activity' => "Leçon 4.1 : Découverte De Symfony" ,
+          'age' => 31
+        ]);
+    }
+
+
     /**
      * @Route("/blog", name="app_blog")
      */
@@ -18,13 +31,25 @@ class BlogController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/", name="home")
-     */
-    public Function home() {
-        return $this->render('blog/home.html.twig', [
-          'title' => "bienvenue",
-          'age' => 31
+/**
+ * @Route("/blog/{id}", name="blog_show" )
+  */
+   public Function show( int $id) {
+   
+        return $this->render('blog/show.html.twig', [
+            'id' => $id,
         ]);
-    }
+ }
+//     /**
+//     * @Route("/blog/{id}", name="blog_show" , methods={"GET","HEAD"}, requirements={"id"="\d+"})
+//     */
+//    public Function show( int $id) {
+//     if ($id==1) {
+//         return $this->render('blog/show.html.twig');
+//     } elseif ($id==2) {
+//         return $this->render('blog/show2.html.twig');
+//     } else{
+//         return $this->render('blog/show3.html.twig');
+//     }  
+//  }
 }
